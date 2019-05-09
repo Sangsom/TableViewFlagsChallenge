9f556169501a6c290ca5b12a72cd9a69e2bcf9a2
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-09 21:20:11 +0300

    Added and outputting country population details

--- TableViewFlagsChallenge/ViewController.swift
+++ TableViewFlagsChallenge/ViewController.swift
@@ -9,45 +9,52 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var flags: [Country] = []
+    var countriesList: [Country] = []
     
     struct Country {
         var name: String
         var abbr: String
         var path: String
+        var population: Int
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        flags += [
-            Country(name: "Estonia", abbr: "EE", path: "estonia"),
-            Country(name: "France", abbr: "FR", path: "france"),
-            Country(name: "Germany", abbr: "DE", path: "germany"),
-            Country(name: "Ireland", abbr: "IE", path: "ireland"),
-            Country(name: "Italy", abbr: "IT", path: "italy"),
-            Country(name: "Monaco", abbr: "MC", path: "monaco"),
-            Country(name: "Nigeria", abbr: "NG", path: "nigeria"),
-            Country(name: "Poland", abbr: "PL", path: "poland"),
-            Country(name: "Russia", abbr: "RU", path: "russia"),
-            Country(name: "Spain", abbr: "ES", path: "spain"),
-            Country(name: "United Kingdom", abbr: "UK", path: "uk"),
-            Country(name: "United States", abbr: "US", path: "us")
+        countriesList += [
+            Country(name: "Estonia", abbr: "EE", path: "estonia", population: 1_316_000),
+            Country(name: "France", abbr: "FR", path: "france", population: 66_990_000),
+            Country(name: "Germany", abbr: "DE", path: "germany", population: 82_790_000),
+            Country(name: "Ireland", abbr: "IE", path: "ireland", population: 4_784_000),
+            Country(name: "Italy", abbr: "IT", path: "italy", population: 60_590_000),
+            Country(name: "Monaco", abbr: "MC", path: "monaco", population: 38_695_000),
+            Country(name: "Nigeria", abbr: "NG", path: "nigeria", population: 190_900_000),
+            Country(name: "Poland", abbr: "PL", path: "poland", population: 38_430_000),
+            Country(name: "Russia", abbr: "RU", path: "russia", population: 144_500_000),
+            Country(name: "Spain", abbr: "ES", path: "spain", population: 46_720_000),
+            Country(name: "United Kingdom", abbr: "UK", path: "uk", population: 66_040_000),
+            Country(name: "United States", abbr: "US", path: "us", population: 327_200_000)
         ]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return flags.count
+        return countriesList.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let flag = flags[indexPath.row]
+        // Format population into decimals
+        let numberFormatter = NumberFormatter()
+        numberFormatter.usesGroupingSeparator = true
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.locale = Locale.current
+        
+        let country = countriesList[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Flags", for: indexPath)
         
-        cell.imageView?.image = UIImage(named: flag.path)
-        cell.textLabel?.text = flag.name
-        cell.detailTextLabel?.text = "This is secondary label"
+        cell.imageView?.image = UIImage(named: country.path)
+        cell.textLabel?.text = country.name
+        cell.detailTextLabel?.text = "Population: \(numberFormatter.string(from: NSNumber(value: country.population))!)"
         return cell
     }
 
