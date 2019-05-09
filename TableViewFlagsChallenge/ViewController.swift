ab961dab0627a75970e88eb5f3b5ec55e1b2872c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-09 14:47:43 +0300

    Added flag names to variable and outputting into table cells

--- TableViewFlagsChallenge/ViewController.swift
+++ TableViewFlagsChallenge/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var flags: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        flags += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flags", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
     }
 
 
