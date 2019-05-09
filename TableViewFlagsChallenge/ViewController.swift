0b5b552ad1008d74a324886e011538e822b2837f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-09 14:23:17 +0300

    Initial Commit

---
+++ TableViewFlagsChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewFlagsChallenge
+//
+//  Created by Rinalds Domanovs on 09/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
